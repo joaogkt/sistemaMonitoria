76b7058676252079df5e87033ce859261e7d19d8
joaogkt <jgabriel.torres@hotmail.com>
2025-07-21 11:47:24 -0300

    User controller e basic auth (temporario) criados

--- src/main/java/org/monitoria/sistemamonitoria/Controllers/UserController.java
+++ src/main/java/org/monitoria/sistemamonitoria/Controllers/UserController.java
@@ -1,17 +1,46 @@
 package org.monitoria.sistemamonitoria.Controllers;
 
+import jakarta.validation.Valid;
+import org.monitoria.sistemamonitoria.DTO.UserDTO;
+import org.monitoria.sistemamonitoria.Models.User;
 import org.monitoria.sistemamonitoria.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 @RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.findAll();
+    };
 
+    @GetMapping("/{id}")
+    public Optional<User> getUserById(@PathVariable long id) {
+        return userService.findById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody UserDTO userDTO) {
+        return userService.registerUser(userDTO);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable long id, @Valid @RequestBody UserDTO userDTO) {
+        return userService.updateUser(id, userDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable long id) {
+        userService.deleteUser(id);
+    }
 
 }
